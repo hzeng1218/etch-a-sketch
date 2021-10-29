edb11b2b2eb1f95afa7e38b8b17677c86a352bb9
hzeng1218 <hzeng1218@gmail.com>
2021-10-29 12:54:01 -0700

    updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+//creating button that clears the grid board
+
+const button = document.createElement('button');
+button.addEventListener('click', () => //;
+body.appendChild(button);
+
 //creating a container div for a grid layout of divs and appends it onto the body
 const body = document.querySelector('body');
 const div = document.createElement('div');
@@ -6,14 +12,16 @@ div.classList.add('container');
 div.setAttribute('style', 'display: flex;')
 body.appendChild(div);
 
+
 //this creates 16 columns of divs.
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < parseInt(size); i++) {
     let columnParts = document.createElement('div');
-        columnParts.classList.add(`grid`);
+        //columnParts.classList.add(`grid`);
         columnParts.textContent = i;
         div.appendChild(columnParts);
+}
 //this creates 16 rows of divs for each column
-    for(let j = 0; j < 15; j++){
+    for(let j = 0; j < parseInt(size) - 1; j++){
         let rowParts = document.createElement('div');
         rowParts.classList.add(`grid`);
         rowParts.textContent = j;
@@ -21,12 +29,8 @@ for(let i = 0; i < 16; i++) {
 }
 const grids = document.querySelectorAll('.grid');
 grids.forEach((grid) => {
-    grid.addEventListener('mouseover', () =>{
-        if (grid.style['background-color'] === 'white'){
+    grid.addEventListener('mouseenter', () =>{
             grid.setAttribute('style', 'background-color: black;')
-        }
-        else grid.setAttribute('style', 'background-color: white;')
     }
     )
-})
-}
+}) 
